4db7d0650871e6d2413dc648d68afc848ab8ecd7
Luan Souza de Almeida <72097035+luanzsouza@users.noreply.github.com>
2022-05-19 20:33:59 -0300

    rotas

--- inno/src/App.js
+++ inno/src/App.js
@@ -1,31 +1,21 @@
-import data from "./data";
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
+    <BrowserRouter>
     <div >
       <header >
      <a href='/'>Inno</a>
       </header>
       <main>
-        <h1>Featured Products</h1>
-       <div className="products">
-        {data.products.map(product =>(
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name}/>
-              </a>
-              <div className="product-info">
-              <a href={`/product/${product.slug}`}>
-              <p>{product.name}</p>
-              </a>
-              <p><strong>${product.price}</strong></p>
-              <button>Add to Cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
+        <Routes>
+          <Route path="/" element={<HomeScreen />}/>
+        </Routes>
       </main>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
