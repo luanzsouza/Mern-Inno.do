9d810552a3f855f0ef4d3e9c118536379c127acb
Luan Souza de Almeida <72097035+luanzsouza@users.noreply.github.com>
2022-05-23 17:05:45 -0300

    install nodemon

---
+++ backend/data.js
@@ -0,0 +1,58 @@
+const data ={
+
+    products : [
+        {
+            name:'Nike Slim Shirt',
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/nike1.png', 
+            price:120,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description: 'higth quality shirt',
+
+        },
+        {
+            name:'Adidas Slim Shirt',
+            slug:'adidas-slim-shirt',
+            category:'Shirts',
+            image:'/images/adidas1.jpg',
+            price:250,
+            countInStock:10,
+            brand:'Adidas',
+            rating:4.0,
+            numReviews:10,
+            description: 'higth quality shirt',
+
+        },
+        {
+            name:'Nike Fit Pant',
+            slug:'nike-fit-pant',
+            category:'Pant',
+            image:'/images/nike2.png',
+            price:25,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:14,
+            description: 'higth quality shirt',
+
+        },
+        {
+            name:'Adidas Fit Pant',
+            slug:'adidas-fit-pant',
+            category:'Pant',
+            image:'/images/adidas2.png',
+            price:30,
+            countInStock:10,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:14,
+            description: 'higth quality shirt',
+
+        },
+    ]
+};
+export default data;
